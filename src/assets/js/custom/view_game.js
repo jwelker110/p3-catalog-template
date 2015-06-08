dc345e3743d530b29686af0e3d27b5c8204a1df6
Jon Welker <JWelker110@gmail.com>
2015-06-08 14:18:33 -0500

    Add reply field hide/show

--- src/assets/js/custom/view_game.js
+++ src/assets/js/custom/view_game.js
@@ -12,4 +12,26 @@
         hideText: 'Less...'
     });
 
+    $('.reply').on('click', function(event){
+        // get btn that was clicked
+        var btn = $(this);
+        var id = btn.data('actionid');
+        var reply = $('.game-review-reply').filter('[data-replyid=' + id + ']');
+        console.log(btn);
+        console.log(id);
+        console.log(reply);
+        if (reply.data('shown')){
+            // perform ajax call to submit comment
+            // todo
+
+            // rehide the comment field
+            $(reply).css({'display': 'none'});
+            reply.data('shown', false);
+        } else {
+            // show the comment textarea
+            $(reply).css({'display': 'inherit'});
+            reply.data('shown', true);
+        }
+    });
+
 }(jQuery);
