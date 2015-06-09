8d0d93b12c6c11d57487aaee7ef0af5fd586aa88
Jon Welker <JWelker110@gmail.com>
2015-06-09 13:21:36 -0500

    Add text collapser to profile page

--- src/assets/js/custom/view_profile.js
+++ src/assets/js/custom/view_profile.js
@@ -3,3 +3,16 @@
  * Dependencies: jQuery, jquery.collapser.js
  */
 
++function($){
+
+    // Text-collapser to minimize space taken by reviews/comments
+    $('.text-collapser').collapser({
+        mode: 'lines',
+        truncate: 4,
+        controlBtn: 'text-collapser-btn',
+        showText: 'More...',
+        hideText: 'Less...'
+    });
+
+}(jQuery);
+
